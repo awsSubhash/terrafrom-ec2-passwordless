da22380b44dfffbb70c80b3e9e3755f0b82f2d53
Subhash Kumar Singh <skumarsingh@synamedia.com>
2026-03-17 10:41:36 +0530

    update the bucket name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-subhash"
-    key            = "eks/terraform.tfstate"
+    key            = "ec2/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
